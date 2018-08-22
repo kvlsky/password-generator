f303b0ae6f0ec57222d1cd93970b177b91687654
kvlsky <a.koval@kendaxa.com>
2018-08-22 15:23:53 +0200

    lenght of the password option added

--- pass.py
+++ pass.py
@@ -6,11 +6,19 @@ import sys
 
 website_ = input("enter the name of the website: ")
 login_ = input("enter your login: ")
+size_ = input("enter password size: ")
+
+while(int(size_) < 8):
+    print("password is too short!")
+    size_ = input("enter size for password: ")
+    if int(size_) >= 8:
+        break
+
 
 filename = "names.csv"
 file_exists = os.path.isfile(filename)
 
-def id_generator(size=10, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation):
+def id_generator(size=int(size_), chars=string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation):
     return ''.join(random.SystemRandom().choice(chars) for _ in range(size))
 
 class NewPass():
@@ -20,14 +28,14 @@ class NewPass():
         try:
             open(filename, "x")
         except:
-             print("File exists:")
+             print("\nFile exists:",filename)
         with open(filename, 'a', newline='', encoding='utf-8') as csvfile:
             fieldnames = ['web_site','login', 'pass']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             if not file_exists:
                 writer.writeheader()
             writer.writerow({'web_site': self.site , 'login': self.login , 'pass': id_generator()})
-        print("new pass created!")
+        print("\nnew pass created!")
 
 newpassword = NewPass(website_,login_)
 
